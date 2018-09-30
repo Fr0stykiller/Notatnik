6e652f50bad6355e3482cac2b1eba797a847552b
Michal <spidey134@gmail.com>
2018-09-30 12:05:11 +0200

    Dodano polaczenie z baza danych. Proste wpisywanie notatki.

--- Notatnik.py
+++ Notatnik.py
@@ -1,6 +1,9 @@
 from tkinter import *
 import tkinter as tk
 import tkinter.scrolledtext as tkscrolled
+import sqlite3
+
+conn = sqlite3.connect('python.db')
 
 
 class Notatnik():
@@ -18,19 +21,30 @@ class Notatnik():
                            text="ODCZYTAJ NOTATKĘ", font=('Calibri Light', 20))
         self.btn2.pack(fill=BOTH, expand=1)
 
+        self.c = conn.cursor()
+        self.c.execute('''CREATE TABLE IF NOT EXISTS notatki
+             (data text, nazwa text, notatka text)''')
+
     def new_note(self):
         self.newWindow = tk.Toplevel(self.master)
-        self.textfield = tkscrolled.ScrolledText(self.newWindow )
+        self.textfield = tkscrolled.ScrolledText(self.newWindow)
         self.textfield.pack()
-        self.addButton = Button(self.newWindow, text="Dodaj!")
+        self.addButton = Button(
+            self.newWindow, text="Dodaj!", command=self.dodaj)
         self.addButton.pack(side=BOTTOM)
 
+    def dodaj(self):
+        self.notka = self.textfield.get("1.0", END)
+        print(self.notka)
+        self.c.execute('''INSERT INTO notatki (data, nazwa, notatka) VALUES ('dzis', 'Notatka1', ?)''', (self.notka,))
+        conn.commit()
 
-root = tk.Tk()
+
+root=tk.Tk()
 Notatnik(root, "400", "300")
-#top1 = Toplevel()
-#top2 = Toplevel()
-#TopLevelWindow(top1, "300", "300")
-#TopLevelWindow(top2, "500", "250")
+# top1 = Toplevel()
+# top2 = Toplevel()
+# TopLevelWindow(top1, "300", "300")
+# TopLevelWindow(top2, "500", "250")
 
 root.mainloop()
