cf2e2be71ad8de4f40620db5c69f968315a5483d
Michal <spidey134@gmail.com>
2018-10-21 21:10:17 +0200

    nowe opcje szukania

--- Notatnik.py
+++ Notatnik.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import tkinter as tk
 import tkinter.scrolledtext as tkscrolled
 import sqlite3
+from tkcalendar import Calendar, DateEntry
 
 conn = sqlite3.connect('python.db')
 
@@ -46,8 +47,25 @@ class Notatnik():
     def odczytaj(self):
         data = self.readfromdatabase()
         self.newWindow1 = tk.Toplevel(self.master)
-        self.labelOdczyt = tk.Label(self.newWindow1, text=data, font=('Calibri Light', 15))
-        self.labelOdczyt.pack()
+        self.ramka1 = tk.Frame(self.newWindow1, bg='blue', bd=2)
+        self.ramka1.grid()
+        self.szukajd = tk.Label(self.ramka1, text="SZUKAJ PO DACIE", font=('Calibri Light', 15))
+        self.szukajd.grid(column=0, row=0)
+        self.szukajde = Calendar(self.ramka1)
+        self.szukajde.grid(column=0, row=1)
+        self.szukajdb = Button(self.ramka1, text="Szukaj!")
+        self.szukajdb.grid(column=0, row=2)
+        self.separator = Frame(self.newWindow1, height=2, bd=1)
+        self.separator.grid(column=1, row = 4)
+        self.szukajt = tk.Label(self.newWindow1, text="SZUKAJ PO TYTULE", font=('Calibri Light', 15))
+        self.szukajt.grid(column=0, row=5)
+        self.szukajte = tk.Entry(self.newWindow1, width=40)
+        self.szukajte.grid(column=0, row=6)
+        self.szukajtb = Button(self.newWindow1, text="Szukaj!")
+        self.szukajtb.grid(column=0, row=7)
+        #self.labelOdczyt = tk.Label(self.newWindow1, text=data, font=('Calibri Light', 15))
+        #self.labelOdczyt.grid(column=0, row=3)
+        
         
     
     def dodaj(self):
