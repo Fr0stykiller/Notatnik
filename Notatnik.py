6f50f9051122ae64f4452614a8636dccea610647
Ola <haszkaa@gmail.com>
2019-02-27 21:11:37 +0100

    naprawa lambdy

--- Notatnik.py
+++ Notatnik.py
@@ -16,11 +16,11 @@ class Notatnik():
         self.master.geometry("%sx%s+100+100" % (width, height))
         self.master.title("Notatnik")
 
-        self.btn = Button(master, bg="lightblue", text="DODAJ NOTATKĘ", font=(
+        self.btn = Button(master, bg="dark slate gray", text="DODAJ NOTATKĘ", font=(
             'Calibri Light', 20), command=self.new_note)
         self.btn.pack(fill=BOTH, expand=1)
 
-        self.btn2 = Button(master, bg="lightblue",
+        self.btn2 = Button(master, bg="rosybrown3",
                            text="ODCZYTAJ NOTATKĘ", font=('Calibri Light', 20), command=self.odczytaj)
         self.btn2.pack(fill=BOTH, expand=1)
 
@@ -38,7 +38,7 @@ class Notatnik():
         self.textfield = tkscrolled.ScrolledText(self.newWindow)
         self.textfield.grid(columnspan=2)
         self.addButton = Button(
-            self.newWindow, text="DODAJ!", bg="LIGHTPINK", font=('Calibri Light', 20), command=self.dodaj)
+            self.newWindow, text="DODAJ!", bg="dark slate gray", font=('Calibri Light', 20), command=self.dodaj)
         self.addButton.grid(columnspan=2)
 
     def searchDatabase(self, searchQuery):
@@ -59,7 +59,7 @@ class Notatnik():
         self.szukajd.grid(column=0, row=1)
         self.szukajde = Calendar(self.ramka1)
         self.szukajde.grid(column=0, row=2)
-        self.szukajdb = Button(self.ramka1, text="SZUKAJ!", bg="slategray2", font=('Calibri Light', 15), command=self.wyniki2)
+        self.szukajdb = Button(self.ramka1, text="SZUKAJ!", bg="rosybrown3", font=('Calibri Light', 15), command=self.wyniki2)
         self.szukajdb.grid(column=0, row=4)
         self.separator = Frame(self.newWindow1, height=3, width=300, bg="slategray4")
         self.separator.grid(column=0, row=5)
@@ -70,7 +70,7 @@ class Notatnik():
         self.szukajte = tk.Entry(self.newWindow1, width=40)
         self.szukajte.grid(column=0, row=7)
         self.szukajtb = Button(
-            self.newWindow1, text="SZUKAJ!", bg="slategray2", font=('Calibri Light', 15), command=self.wyniki)
+            self.newWindow1, text="SZUKAJ!", bg="rosybrown3", font=('Calibri Light', 15), command=self.wyniki)
         self.szukajtb.grid(column=0, row=9)
 
         self.newWindow1.grid_rowconfigure(3, minsize=10)
@@ -93,21 +93,27 @@ class Notatnik():
         self.newWindow4 = tk.Toplevel(self.master)
         lista2 = self.searchDatabase2(self.szukajde.get_date())
         for record in lista2:
-            recordbutton = Button(self.newWindow4, text=record[1], bg="slategray2", font=('Calibri Light', 15))
-            recordbutton.pack()
-
+            recordbutton = Button(self.newWindow4, text=record[1], bg="gray80", font=('Calibri Light', 15), command=lambda record=record: self.pokazNotatke(record[2]))
+            recordbutton.pack(fill=X)
+    
     def wyniki(self):
         self.newWindow2 = tk.Toplevel(self.master)
         self.resultLabel = tk.Label(self.newWindow2, text=(str(self.searchDatabase(self.szukajte.get()))))
-        self.lista = self.searchDatabase(self.szukajte.get())
-        for record in self.lista:
-            recordbutton = Button(self.newWindow2, text=record[1] +"|" + record[0], bg="slategray2", font=('Calibri Light', 15), command=lambda: self.pokazNotatke(record[2]))
-            recordbutton.pack()
+        lista = self.searchDatabase(self.szukajte.get())
+        for record in lista:
+            recordbutton = Button(self.newWindow2, text=record[1] +"|" +record[0], bg="gray80", font=('Calibri Light', 15), command=lambda: self.pokazNotatke(record[2]))
+            recordbutton.pack(fill=X)
 
     def pokazNotatke(self, Notka):
         self.newWindow3 = tk.Toplevel(self.master)
-        self.notatkaLabel = Label(self.newWindow3, text = Notka)
-        self.notatkaLabel.pack()
+        self.newWindow3.geometry("500x400")
+        self.pokaztytul = tk.Label(self.newWindow3, text=Notka, font=('Calibri Light', 20, "bold", "underline"))
+        self.pokaztytul.grid(column=0, row=0, sticky='w')
+        self.newWindow3.grid_rowconfigure(1, minsize=15)
+        self.notatkaLabel = Label(self.newWindow3, text = Notka, font=('Calibri Light', 15))
+        self.notatkaLabel.grid(column=0, row=2, sticky='w')
+        
+    
 
     def dodaj(self):
         self.notka = self.textfield.get("1.0", END)
